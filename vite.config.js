bde82ce3d46aa8910761bfb49368d587979d82f7
Joaquin Bujakiewicz <joabuja@gmail.com>
2023-09-01 14:38:42 -0300

    arreglados iconos y ajustadas paginas contacto y nosotros

--- vite.config.js
+++ vite.config.js
@@ -16,7 +16,17 @@ export default {
                 carrito: resolve('pages/carrito.html'),
                 contacto: resolve('pages/contacto.html'),
                 nosotros: resolve('pages/nosotros.html'),
-                index: resolve('index.html')
+                index: resolve('index.html'),
+                asus1: resolve('pages/asus-zb631.html'),
+                asus2: resolve('pages/asus-zenfone-8.html'),
+                asus3: resolve('pages/asus-zenfone-3-max.html'),
+                lg1: resolve('pages/lgk22plus.html'),
+                lg2: resolve('pages/lgk52.html'),
+                lg3: resolve('pages/lgk62plus.html'),
+                Moto1: resolve('pages/Motorola-E13.html'),
+                Moto2: resolve('pages/MotoG42.html'),
+                Moto3: resolve('pages/MotoEdge40.html'),
+
             }
         }
     }
